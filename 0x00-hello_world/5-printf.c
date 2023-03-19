0982e790b3eb28d1deee625062c3c82cbe5686ea
sadickseidu <sadickseidu8@gmail.com>
2023-03-19 06:30:43 -0700

    commit on all task Hellow world in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - contains the main function of programme
+*
+*Return: returns something back
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
